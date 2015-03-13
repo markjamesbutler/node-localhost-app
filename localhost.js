d7061264fd75f3a8ba2d7769e76209797cd5ad02
markjamesbutler <markjamesbutler@gmail.com>
2015-03-13 15:33:06 +0000

    Add count.

--- localhost.js
+++ localhost.js
@@ -1,6 +1,8 @@
 var http = require('http');
+var count = 0
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
+  console.log('GET ' + count++);
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
